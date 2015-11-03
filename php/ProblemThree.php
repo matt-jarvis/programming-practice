c00842d19300dcdb68c9372968d6c822d4297f7f
matt-jarvis <mattjarvis659@gmail.com>
2015-11-03 12:45:35 +0000

    Problem three: amend fibonacci function to handle large numbers

--- php/ProblemThree.php
+++ php/ProblemThree.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Write a function that computes the list of the first 100 
  * Fibonacci numbers. By definition, the first two numbers in 
@@ -14,22 +15,31 @@
 
 
 /* 
- * Computes a list of Fibonacci numbers.
+ * Computes a list of Fibonacci numbers. 
  *
  * @param length: The amount of Fibonacci numbers to compute.
- * @return: The list of Fibonacci numbers.
+ * @return: The list of Fibonacci numbers (as strings).
  *
- * TODO: Handle big numbers where length > 92
+ * Note: Fibonacci numbers get big quickly, so big that they exceed
+ *       the largest number an int or even a long can represent. For
+ *       this reason, we store the numbers as a string and add them 
+ *       using the bcadd function of the bcmath library.
  */
 function fibonacci($length)
 {
-	$c = array(0, 1);	
+	$fib = array('0', '1');	
 	
 	// Compute the Fibonacci numbers.
-	while (count($c) != $length)
-		$c[] = $c[count($c) - 1] + $c[count($c) - 2];
+	$fib_len = count($fib);
+	while ($fib_len < $length)
+	{
+		$a = $fib[$fib_len - 1];
+		$b = $fib[$fib_len - 2];
+		$fib[] = bcadd($a, $b);
+		$fib_len = count($fib);
+	}
 	
-	return array_slice($c, 0, $length);
+	return array_slice($fib, 0, $length);
 }
 
 
