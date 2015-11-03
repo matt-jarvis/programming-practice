39440c324e56eebd3859fe6cdd77cf091812f8de
matt-jarvis <mattjarvis659@gmail.com>
2015-11-03 13:26:47 +0000

    Problem three: implement fibonacci function (see TODO)

--- c/problemthree.c
+++ c/problemthree.c
@@ -10,9 +10,17 @@
 ** 
 ** Author:      Matthew Jarvis        
 ** Created:     2nd Nov, 2015
+**
+** TODO:        It seems the largest built-in data type in c is an unsigned 
+**              long long, which still isn't large enough to represent all 
+**              the Fibonacci numbers we require (the 93rd is the largest 
+**              number that can represented by an unsigned long long. 
+**              Perhaps try a library?
 ** --------------------------------------------------------------------------*/
 #include <stdio.h>
- 
+#include <stdlib.h>
+
+typedef unsigned long long LLU;
  
 /* 
  * Function: fibonacci
@@ -22,13 +30,32 @@
  * length: The amount of Fibonacci numbers to compute.
  * returns: The list of Fibonacci numbers.
  */
-int *fibonacci(int length)
+LLU *fibonacci(int length)
 {	
-	static int fib[] = { 0, 1 };
+	LLU *fib = malloc(length * sizeof(LLU));
+	int i = 0;
 	
-	// TODO: compute the Fibonacci numbers.
+	// Add the first element to the array.
+	if (length > i)
+	{
+		fib[i] = 0;
+		i++;
+	}
 	
-	// TODO: only return an array of length size.
+	// Add the second element to the array.
+	if (length > i)
+	{
+		fib[i] = 1;
+		i++;
+	}
+	
+	// Now compute the Fibonacci numbers.
+	while(i < length)
+	{
+		fib[i] = fib[i - 1] + fib[i - 2];
+		i++;
+	}
+		
 	return fib;
 }
 
@@ -37,9 +64,9 @@ int main()
 {
 	// Initialise and compute Fibonacci numbers
 	int fiblen = 100;
-	int *fib = fibonacci(fiblen);
+	LLU *fib = fibonacci(fiblen);
 	
 	// Print Fibonnaci numbers
 	for (int i = 0; i < fiblen; i++)
-		printf("Fibonnaci %d: \t %d \n", i, *(fib + i));
+		printf("Fibonnaci %d: \t %llu \n", i, *(fib + i));
 }
